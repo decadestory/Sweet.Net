019d09337b13d089dbd0178fd6dd956499d941dd
decadestory <728246447@qq.com>
2016-06-21 12:29:30 +0800

    Add FormatTimeSpan

--- Sweet.Net/Ext/DataTimeExt.cs
+++ Sweet.Net/Ext/DataTimeExt.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Sweet.Net.Ext
 {
-    class DataTimeExt
+    public static class DataTimeExt
     {
+        /// <summary>
+        /// 格式化TimeSpan: xx天xx时xx分xx秒xx毫秒
+        /// </summary>
+        /// <param name="milliSeconds">毫秒数</param>
+        /// <returns></returns>
+        public static string FormatTimeSpan(this long milliSeconds)
+        {
+            var span = TimeSpan.FromMilliseconds(milliSeconds);
+            var day = span.Days == 0 ? string.Empty : span.Days + "天";
+            var hour = span.Hours == 0 ? string.Empty : span.Hours + "时";
+            var min = span.Minutes == 0 ? string.Empty : span.Minutes + "分";
+            var sec = span.Seconds == 0 ? string.Empty : span.Seconds + "秒";
+            var ms = span.Milliseconds == 0 ? string.Empty : span.Milliseconds + "毫秒";
+            return day + hour + min + sec + ms;
+        }
     }
 }
